899ca41d8d25f124f1dc2179bf7830f2dc2f8746
Juarez Martins <juniorbmx13@gmail.com>
2022-10-26 20:10:30 -0300

    bug: não lê o documento db.properties

--- src/main/java/br/com/ormtraining/jdbc/db/DB.java
+++ src/main/java/br/com/ormtraining/jdbc/db/DB.java
@@ -14,7 +14,8 @@ public class DB {
 
 
     private static Properties loadProperties() {
-        try (FileInputStream fs = new FileInputStream("db.properties")) {
+        try {
+            FileInputStream fs = new FileInputStream("db.properties");
             Properties props = new Properties();
             props.load(fs);
             return props;
